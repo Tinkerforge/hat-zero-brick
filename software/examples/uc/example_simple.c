46ee6c1c53b9b1f570c4a58c66fd26dce61764a2
Erik Fleckstein <erik@tinkerforge.com>
2020-08-26 10:35:54 +0200

    uc: Fix whitespace

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -11,13 +11,11 @@ void example_setup(TF_HalContext *hal) {
 	// Create device object
 	check(tf_hat_zero_create(&hz, UID, hal), "create device object");
 
-
 	// Get current voltage
 	uint16_t voltage;
 	check(tf_hat_zero_get_usb_voltage(&hz, &voltage), "get voltage");
 
 	tf_hal_printf("Voltage: %d 1/%d V\n", voltage, 1000.0);
-
 }
 
 void example_loop(TF_HalContext *hal) {
