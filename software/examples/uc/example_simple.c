dc62a63c2a0f231fd72c11b11bf53268850f8e30
Erik Fleckstein <erik@tinkerforge.com>
2022-04-19 16:03:20 +0200

    examples: uc: Rename TF_HalContext to TF_HAL.

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -9,13 +9,13 @@
 
 void check(int rc, const char* msg);
 
-void example_setup(TF_HalContext *hal);
-void example_loop(TF_HalContext *hal);
+void example_setup(TF_HAL *hal);
+void example_loop(TF_HAL *hal);
 
 
 static TF_HATZero hz;
 
-void example_setup(TF_HalContext *hal) {
+void example_setup(TF_HAL *hal) {
 	// Create device object
 	check(tf_hat_zero_create(&hz, UID, hal), "create device object");
 
@@ -26,7 +26,7 @@ void example_setup(TF_HalContext *hal) {
 	tf_hal_printf("Voltage: %d 1/%d V\n", voltage, 1000);
 }
 
-void example_loop(TF_HalContext *hal) {
+void example_loop(TF_HAL *hal) {
 	// Poll for callbacks
 	tf_hal_callback_tick(hal, 0);
 }
